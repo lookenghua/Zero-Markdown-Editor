3ee5625a404bb2ffd00a3d722dc14dca0822bbf1
lookenghua <lookenghua@gmail.com>
2022-03-17 20:30:38 +0800

    feat: 增加脚注功能

--- src/lib/viewer.svelte
+++ src/lib/viewer.svelte
@@ -5,29 +5,51 @@
     import rehypeStringify from 'rehype-stringify'
     import remarkBreaks from 'remark-breaks'
     import remarkParse from 'remark-parse'
-    import remarkRehype from 'remark-rehype'
-    import {defaultSchema,Schema} from 'hast-util-sanitize'
+    import rehypePrism from '@mapbox/rehype-prism'
+    import remarkRehype from "remark-rehype"
+    import remarkFootnotes from 'remark-footnotes'
+    import {defaultSchema} from 'hast-util-sanitize'
+    import deepmerge from 'deepmerge'
+    import rehypeAutolinkHeadings from "rehype-autolink-headings"
 
     export let value = ''
     let html = ''
     const schemaStr = JSON.stringify(defaultSchema)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const schema: Schema = JSON.parse(schemaStr)
+    const schema = deepmerge(JSON.parse(schemaStr), {
+        attributes: {
+            pre: ['className'],
+            code: ['className']
+        }
+    })
+
+    function debug(method) {
+        console.log(method)
+        return (tree) => {
+            console.log(tree)
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+            return tree
+        }
+    }
 
     function updateHTML() {
         unified()
-            .use(remarkParse) // 解析markdown文本
-            .use(remarkBreaks)
-            .use(remarkRehype, {allowDangerousHtml: true})
-            .use(rehypeRaw)
-            .use(rehypeSanitize, schema)
-            .use(rehypeStringify)
+            .use(remarkParse)// 解析markdown文本
+            .use(remarkBreaks) // 保持换行状态
+            .use(remarkFootnotes, {inlineNotes: true})
+            .use(remarkRehype, {allowDangerousHtml: true}) // 将markdown转换成属性结构
+            .use(rehypePrism) // 高亮代码
+            .use(rehypeRaw)// 保持markdown里面直接书写的html
+            .use(rehypeSanitize, schema)// 过滤html字符串
+            .use(rehypeStringify) // 将AST树形结构转换成html
+            .use(rehypeAutolinkHeadings)
             .process(value)
             .then((vfile) => String(vfile))
             .then((str) => (html = str))
     }
 
     $: {
+        console.log(value)
         updateHTML()
     }
 </script>
